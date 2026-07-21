b612e576b8dac57fe8e82dad45a8a77df1b89ab4
Mahmoud Gohar <goharmahmoud987@gmail.com>
2026-07-22 02:24:06 +0300

    Finish HomeView

--- lib/features/home/presentation/views/widgets/Custom_home_app_bar.dart
+++ lib/features/home/presentation/views/widgets/Custom_home_app_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:fruitify/core/assets.dart';
+import 'package:fruitify/core/helper/get_user.dart';
 import 'package:fruitify/core/utils/app_text_styles.dart';
 
 class CustomHomeAppBar extends StatelessWidget {
@@ -8,9 +9,11 @@ class CustomHomeAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final user = getUser();
+
     return ListTile(
       trailing: Container(
-        padding: EdgeInsets.all(12),
+        padding: const EdgeInsets.all(12),
         decoration: const ShapeDecoration(
           color: Color(0XFFEEF8ED),
           shape: OvalBorder(),
@@ -20,15 +23,12 @@ class CustomHomeAppBar extends StatelessWidget {
       leading: Image.asset(Assets.assetsImagesProfileImage),
       title: Text(
         'صباح الخير !..',
-        textAlign: TextAlign.right,
         style: TextStyles.regular16.copyWith(color: const Color(0XFF949D9E)),
       ),
       subtitle: Text(
-        'أحمد مصطفي',
-        textAlign: TextAlign.right,
+        user?.name ?? '',
         style: TextStyles.bold16.copyWith(color: const Color(0xFF0C0D0D)),
       ),
-    
     );
   }
 }
